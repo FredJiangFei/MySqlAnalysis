5ce74a4c08f6f33bafac7542759fec6b71320e2f
Fred Jiang <jiangf@shinetechchina.com>
2020-05-15 17:10:13 +0800

    build

---
+++ SqlAnalysis/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SqlAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var script = "SELECT UserName, Password FROM Account";
+            var path = @"E:\MySolutions\MySqlAnalysis\SqlAnalysis\Files\test.sql";
+            var script = File.ReadAllText(path);
+
+            var fragment = SqlParser.ParseScript(script);
+            //var s = fragment.OfType<DeleteStatement>().FirstOrDefault();
+            foreach (TSqlBatch batch in fragment.Batches)
+            {
+                if (batch.Statements.Count == 0) continue;
+
+                foreach (TSqlStatement statement in batch.Statements)
+                {
+                    foreach (var token in statement.ScriptTokenStream)
+                    {
+                        if(token.TokenType == TSqlTokenType.Identifier)
+                        {
+                            Console.WriteLine(token.Text);
+                        }
+                    }
+                }
+            }
+
+        }
+
+       
+    }
+}
---
+++ SqlAnalysis/SqlParser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SqlAnalysis
+{
+   public class SqlParser
+    {
+        public static TSqlScript ParseScript(string script)
+        {
+            IList<ParseError> parseErrors;
+            TSqlParser tsqlParser = new TSql140Parser(true);
+            TSqlFragment fragment;
+            using (var stringReader = new StringReader(script))
+            {
+                fragment = tsqlParser.Parse(stringReader, out parseErrors);
+            }
+            return (TSqlScript)fragment;
+        }
+    }
+}
